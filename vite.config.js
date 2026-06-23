a99f46f59c64845dc5df03f087c3a40daef6bc8c
Debora1Allen <deboraaaline21@gmail.com>
2026-06-23 17:41:50 -0300

    config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/portifolio/",
+  base: "./",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         assombracoes: resolve(__dirname, "/pages/leia-assombracoes/index.html"),
-        literario:  resolve(__dirname,"/pages/portifolio-literario/index.html"),
-        contato:  resolve(__dirname,"/pages/contato/index.html"),
-        livros:  resolve(__dirname,"/pages/livros/index.html"),
+        literario: resolve(__dirname, "/pages/portifolio-literario/index.html"),
+        contato: resolve(__dirname, "/pages/contato/index.html"),
+        livros: resolve(__dirname, "/pages/livros/index.html"),
       },
     },
   },
