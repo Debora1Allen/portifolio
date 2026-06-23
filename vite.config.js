a2a9113cb2bfae49db35844923c6414dc5ee6b80
Debora1Allen <deboraaaline21@gmail.com>
2026-06-23 03:32:43 -0300

    01101110 01100101 01110110 01100101 01110010 01101101 01101111 01110010 01100101

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/portifolio/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        assombracoes: "assombracoes.html",
+        literario: "literario.html",
+        contato: "contato.html",
+        livros: "livros.html",
+      },
+    },
+  },
 });
